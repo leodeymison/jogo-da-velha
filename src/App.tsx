d8c8e3e88329c7eb4b49e9515070d52570c5b681
leodeymison <leodeymisonbasquete@gmail.com>
2023-01-10 21:38:04 -0300

    add e remove bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
-import { BoxStyled, ContainerStyled, MessageStyled } from "./style";
+import {
+  BoxStyled,
+  BoxedStyled,
+  ContainerStyled,
+  FormStyled,
+  MessageStyled,
+  ScoreboardStyled,
+} from "./style";
+import { useForm } from "react-hook-form";
+
 const App = () => {
+  const { register, handleSubmit } = useForm();
+  const [addNames, setAddNames] = useState(false);
+
   const [list, setList] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
   const [timePlayer, setTimePlayer] = useState(1);
   const [message, setMessage] = useState("");
+  const [winnerCombo, setWinnerCombo] = useState(0);
+
+  const [players, setPlayers] = useState([
+    {
+      score: 0,
+      name: "Jogador 1",
+    },
+    {
+      score: 0,
+      name: "Jogador 2",
+    },
+  ]);
 
   const SequencesCorrect = [
     [0, 1, 2], //  horizontal
@@ -28,57 +52,123 @@ const App = () => {
     setList(list.map((item) => 0));
     setMessage("");
     setTimePlayer(1);
+    setWinnerCombo(0);
+  };
+
+  const addScorePlayer = (player: 1 | 2) => {
+    setPlayers(
+      players.map((item, index) =>
+        index === player - 1 ? { ...item, score: item.score + 1 } : item
+      )
+    );
+  };
+  const editNamePlayer = (data: any) => {
+    if (!data.name1 || !data.name2) {
+      return;
+    }
+    setPlayers(
+      players.map((item, index) => ({
+        ...item,
+        name: data[`name${index + 1}`],
+      }))
+    );
+    setAddNames(true);
+  };
+
+  const activeMessage = (text: string) => {
+    setMessage(text);
+    setTimeout(() => clearBoard(), 2000);
+  };
+
+  const VerifyEndGame = () => {
+    const end = list.every((item) => item !== 0);
+    if (end) activeMessage("Empate");
+  };
+  const VerifyWinner = () => {
+    var winner = null;
+    for (let item of SequencesCorrect) {
+      if (list[item[0]] === 1 && list[item[1]] === 1 && list[item[2]] === 1) {
+        winner = 1;
+      }
+      if (list[item[0]] === 2 && list[item[1]] === 2 && list[item[2]] === 2) {
+        winner = 2;
+      }
+
+      if (winner === 1 || winner === 2) {
+        setWinnerCombo(winner);
+        addScorePlayer(winner);
+        activeMessage(`Jogador ${winner} venceu`);
+        break;
+      }
+    }
   };
 
   const ClickElement = (index: number) => {
+    if (winnerCombo) {
+      return;
+    }
+    if (list[index] !== 0) {
+      return;
+    }
+
     setList((prev) => {
       const listCurrent = [...prev];
       listCurrent[index] = timePlayer;
       return listCurrent;
     });
 
-    VerifyWinner();
-    VerifyEndGame();
-
     setTimePlayer((current) => (current === 1 ? 2 : 1));
-  };
-
-  const VerifyEndGame = () => {
-    const end = list.every((item) => item !== 0);
-    if (end) {
-      setMessage("Empate");
-      setTimeout(() => clearBoard(), 2000);
-    }
-  };
-  const VerifyWinner = () => {
-    for (let item of SequencesCorrect) {
-      if (list[item[0]] === 1 && list[item[1]] === 1 && list[item[2]] === 1) {
-        setMessage(`jogador ${timePlayer} vencer`);
-        break;
-      }
-      if (list[item[0]] === 2 && list[item[1]] === 2 && list[item[2]] === 2) {
-        setMessage(`jogador ${timePlayer} vencer`);
-        break;
-      }
-    }
   };
 
   return (
     <ContainerStyled>
       {message && <MessageStyled>{message}</MessageStyled>}
 
-      <div>
-        {list.map((item, index) => (
-          <BoxStyled
-            key={index}
-            onClick={() => ClickElement(index)}
-            color={item === 1 ? "x" : "circle"}
-          >
-            {item === 1 && "X"}
-            {item === 2 && "O"}
-          </BoxStyled>
-        ))}
-      </div>
+      {!addNames ? (
+        <FormStyled onSubmit={handleSubmit(editNamePlayer)}>
+          <label>
+            <span>Jogador 1</span>
+            <input
+              {...register("name1")}
+              type="text"
+              placeholder="Digite o seu nome"
+            />
+          </label>
+          <label>
+            <span>Jogador 2</span>
+            <input
+              {...register("name2")}
+              type="text"
+              placeholder="Digite o seu nome"
+            />
+          </label>
+          <label>
+            <button type="submit">Salvar</button>
+          </label>
+        </FormStyled>
+      ) : (
+        <>
+          <ScoreboardStyled>
+            {players.map((item, index) => (
+              <p key={index}>
+                {item.name}: <span>{item.score} pontos</span>
+              </p>
+            ))}
+          </ScoreboardStyled>
+          <BoxedStyled>
+            {list.map((item, index) => (
+              <BoxStyled
+                key={index}
+                onClick={() => ClickElement(index)}
+                color={item === 1 ? "x" : "circle"}
+              >
+                {item === 1 && "X"}
+                {item === 2 && "O"}
+              </BoxStyled>
+            ))}
+          </BoxedStyled>
+        </>
+      )}
     </ContainerStyled>
   );
 };
--- src/style.ts
+++ src/style.ts
@@ -15,14 +15,16 @@ export const ContainerStyled = styled("div")`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   height: 100vh;
-  div {
-    width: 500px;
-    height: 500px;
-    border: solid 2px #eee;
-    display: flex;
-    flex-wrap: wrap;
-  }
+`;
+
+export const BoxedStyled = styled("span")<BoxType>`
+  width: 500px;
+  height: 500px;
+  border: solid 2px #eee;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 type BoxType = {
@@ -55,4 +57,57 @@ export const MessageStyled = styled("span")`
   color: black;
   font-size: 18px;
   border-radius: 10px;
+`;
+
+export const ScoreboardStyled = styled("div")`
+  color: white;
+  width: 500px;
+  padding: 1em 0;
+  p {
+    padding: 0.5em 1em 0.5em 0;
+    span {
+      background-color: blueviolet;
+      padding: 0.3em 0.5em;
+      border-radius: 5px;
+    }
+  }
+`;
+
+export const FormStyled = styled("form")`
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+  background-color: white;
+  color: black;
+  position: absolute;
+  border-radius: 10px;
+  label {
+    display: flex;
+    flex-direction: column;
+    padding: 0.5em 0;
+    span {
+      font-size: 12px;
+      color: #6f6f6f;
+      padding: 0.2em 0;
+    }
+    button {
+      padding: 5px;
+      background-color: #07b2ee;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      &:focus {
+        outline: none;
+      }
+    }
+    input {
+      padding: 5px;
+      border-radius: 5px;
+      border: solid 1px #aaa;
+      &:focus {
+        outline: none;
+      }
+    }
+  }
 `;
