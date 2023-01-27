ccecccda2bc1a7347521d646facc860411ad68bb
leodeymison <leodeymisonbasquete@gmail.com>
2023-01-27 13:50:43 -0300

    test ads

--- src/App.tsx
+++ src/App.tsx
@@ -163,6 +163,20 @@ const App = () => {
           <label>
             <button type="submit">Salvar</button>
           </label>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7603393345727255"
+            crossOrigin="anonymous"
+          ></script>
+          <ins
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-7603393345727255"
+            data-ad-slot="1134077837"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         </FormStyled>
       ) : (
         <>
